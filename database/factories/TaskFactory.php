c9b8c502eb499dcfe7396c2327ad5224ebaa66b8
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-03 14:55:08 -0300

    making seeder and factories

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\TaskStatusEnum;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->words(3, true),
+            'description' => fake()->sentence(),
+            'status' => fake()->randomElement(TaskStatusEnum::values()),
+            'completed_at' => fake()->boolean()
+                ? fake()->dateTimeBetween('-1 month', 'now')
+                : null,
+            'kanban_id' => null
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kanban;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $QUANTITY_OF_TASKS = 10;
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $kanban = Kanban::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        for ($i=0; $i < $QUANTITY_OF_TASKS; $i++) { 
+            Task::factory()->create([
+                'kanban_id' => $kanban->id
+            ]);
+        }
     }
 }
