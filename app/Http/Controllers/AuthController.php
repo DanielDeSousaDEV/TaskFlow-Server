c62ff151504d7ba8ef3297f015ae8a82fcf53c1e
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-03 14:59:18 -0300

    adding auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\Kanban;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    function login(LoginRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $succefulLogged = Auth::attempt([
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password']
+        ], true);
+
+        if (!$succefulLogged) {
+            return response([
+                'success' => false,
+                'message' => 'Credeciais invalidas',
+            ], 403);
+        }
+        
+        return response([
+            'success' => true,
+            'message' => 'Login feito com sucesso',
+            'data' => Auth::user(),
+            'tok' => Auth::user()->createToken('api-token')->plainTextToken
+        ]);
+    }
+
+    function register(RegisterRequest $request) 
+    {
+        $validatedData = $request->validated();
+
+        try {
+            $user = User::create($validatedData);
+
+            $kanban = Kanban::create(['user_id' => $user->id]);
+            
+            return response([
+                'success' => true,
+                'message' => 'Usuário criado com sucesso',
+                'data' => $user
+            ]);
+        } catch (\Throwable $th) {
+            return response([
+                'success' => false,
+                'message' => 'Erro desconhecido',
+            ], 500);
+        }
+    }
+
+    function me ()
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response([
+                'success' => false,
+                'message' => 'Usuário não está logado',
+            ], 401);
+        }
+
+        return response([
+            'success' => true,
+            'message' => 'Usuário está logado',
+            'data' => $user
+        ]);
+    }
+}
