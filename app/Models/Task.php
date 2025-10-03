b987f6f54129594571bb2fa08b7fb7ff545d9787
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-03 14:55:21 -0300

    adding models

---
+++ app/Models/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'completed_at',
+        'kanban_id'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'completed_at' => 'datetime'
+        ];
+    }
+
+    public function kanban()
+    {
+        return $this->belongsTo(Kanban::class);
+    }
+}
