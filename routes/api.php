aa5b51c4661e6c04028fb83ccc293cddfbb54cd3
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-03 15:00:04 -0300

    adding routes

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,19 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
-Route::post('/login' , function () {
-    // Exemplo sem validação, só para testar
-    $user = User::find(1);
-
-    Auth::login($user); // agora sim gera o cookie da sessão
-
-    $tok = $user->createToken('api_token');
-
-    return response()->json([
-        'message' => 'Logged in',
-        'user' => $user,
-        'token' => $tok->plainTextToken
-    ]);
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
 
-Route::get('/check', function () {
-    return Auth::check();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/tasks', TaskController::class);  
+
+    Route::get('/home', [HomeController::class, 'index']);
 });
